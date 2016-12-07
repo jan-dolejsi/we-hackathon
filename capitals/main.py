38d92ef00685b12d6e056fecd7b21de56cae5a3b
Jan Dolejsi <JDolejsi@slb.com>
2016-12-07 10:59:01 -0600

    fixed the sample

--- capitals/main.py
+++ capitals/main.py
@@ -1,19 +1,28 @@
 import logging
+import json
+import base64
 
 from flask import Flask
 from flask_cors import CORS
-import api
 
 
 app = Flask(__name__)
-CORS(app)
 
+@app.route('/')
+def hello_world():
+    """hello world"""
+    return 'Hello World!'
 
-app.register_blueprint(api.api)
-
+@app.route('/test')
+def test():
+    return 'Test!'
 
 @app.errorhandler(500)
 def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
+
+if __name__ == '__main__':
+    # Used for running locally
+    app.run(host='127.0.0.1', port=8081, debug=True)
